d8e83f27d15cfbe19b84e6906f6c402464b8c227
Willem van de Kletersteeg <willem.van.de.kletersteeg@politie.nl>
2023-12-04 22:41:52 +0100

    Refactoring day 3 a little

--- d3/day3.py
+++ d3/day3.py
@@ -4,74 +4,34 @@ SYMBOLS = ["*", "#", "$", "+", "%", "/", "&", "=", "-", "@"]
 
 
 def get_char_at_index(string: str, index: int) -> Union[str, None]:
-    if index < 0:
-        return None
-    elif index > len(string):
+    if index < 0 or index > len(string):
         return None
     else:
         return string[index]
 
 
-def get_valid_part_numbers(lines: str, line_length: int) -> List:
-    part_numbers = []
-    candidate_p_nr = []
-    for i, c in enumerate(lines):
-        if c.isdigit():
-            candidate_p_nr.append(c)
-            continue
-        if c in SYMBOLS and len(candidate_p_nr) == 0:
-            continue
-        elif c in SYMBOLS and len(candidate_p_nr) > 0:
-            parts_number = int("".join(candidate_p_nr))
-            part_numbers.append(parts_number)
-            candidate_p_nr = []
-            continue
-        if c == "." and len(candidate_p_nr) > 0:
-            parts_nr_length = len(candidate_p_nr)
-            parts_number = int("".join(candidate_p_nr))
-
-            cand = get_char_at_index(lines, i - parts_nr_length - 1)
-            if cand and cand in SYMBOLS:
-                part_numbers.append(parts_number)
-                candidate_p_nr = []
-                continue
-
-            for j in range(parts_nr_length + 2):
-                index_down = (i + line_length) - j
-                cand = get_char_at_index(lines, index_down)
-                if cand and cand in SYMBOLS:
-                    part_numbers.append(parts_number)
-                    break
-
-                index_up = (i - line_length) - j
-                cand = get_char_at_index(lines, index_up)
-                if cand and cand in SYMBOLS:
-                    part_numbers.append(parts_number)
-                    break
-            candidate_p_nr = []
-    return part_numbers
-
-
-def add_or_append_gear_entry(gear_list: dict, gear_index: int, parts_number: int):
-    gear_entry = gear_list.get(gear_index)
-    if gear_entry:
-        gear_entry.append(parts_number)
+def add_or_append_part_number(parts_list: dict, symbol_index: int, parts_number: int):
+    parts_entry = parts_list.get(symbol_index)
+    if parts_entry:
+        parts_entry.append(parts_number)
     else:
-        gear_list[gear_index] = [parts_number]
+        parts_list[symbol_index] = [parts_number]
 
 
-def get_adjacent_to_gears(lines: str, line_length: int) -> List:
-    gears = {}
+def get_part_numbers_adjacent_to_symbols(
+    lines: str, line_length: int, symbols: List[str]
+) -> List:
+    parts = {}
     candidate_p_nr = []
     for i, c in enumerate(lines):
         if c.isdigit():
             candidate_p_nr.append(c)
             continue
-        if c == "*" and len(candidate_p_nr) == 0:
+        if c in symbols and len(candidate_p_nr) == 0:
             continue
-        elif c == "*" and len(candidate_p_nr) > 0:
+        elif c in symbols and len(candidate_p_nr) > 0:
             parts_number = int("".join(candidate_p_nr))
-            add_or_append_gear_entry(gears, i, parts_number)
+            add_or_append_part_number(parts, i, parts_number)
             candidate_p_nr = []
             continue
         if c == "." and len(candidate_p_nr) > 0:
@@ -80,25 +40,25 @@ def get_adjacent_to_gears(lines: str, line_length: int) -> List:
 
             index = i - parts_nr_length - 1
             cand = get_char_at_index(lines, index)
-            if cand and cand == "*":
-                add_or_append_gear_entry(gears, index, parts_number)
+            if cand and cand in symbols:
+                add_or_append_part_number(parts, index, parts_number)
                 candidate_p_nr = []
                 continue
 
             for j in range(parts_nr_length + 2):
                 index_down = (i + line_length) - j
                 cand = get_char_at_index(lines, index_down)
-                if cand and cand == "*":
-                    add_or_append_gear_entry(gears, index_down, parts_number)
+                if cand and cand in symbols:
+                    add_or_append_part_number(parts, index_down, parts_number)
                     break
 
                 index_up = (i - line_length) - j
                 cand = get_char_at_index(lines, index_up)
-                if cand and cand == "*":
-                    add_or_append_gear_entry(gears, index_up, parts_number)
+                if cand and cand in symbols:
+                    add_or_append_part_number(parts, index_up, parts_number)
                     break
             candidate_p_nr = []
-    return gears
+    return parts
 
 
 def get_gear_ratios(gears: dict) -> List:
@@ -110,6 +70,14 @@ def get_gear_ratios(gears: dict) -> List:
     return list_of_gear_ratios
 
 
+def get_sum_of_all_parts(parts_list: dict) -> int:
+    sum_part_nrs = 0
+    for symbol in parts_list:
+        for part_number in parts_list[symbol]:
+            sum_part_nrs += part_number
+    return sum_part_nrs
+
+
 def day3():
     with open("d3/input1.txt", "r") as file:
         lines = file.read()
@@ -117,10 +85,11 @@ def day3():
     line_length = len(lines[0])
     lines = "".join(lines)
 
-    part_numbers = get_valid_part_numbers(lines, line_length)
-    print(f"Part one: sum of all part numbers = {sum(part_numbers)}")
+    all_symbols = get_part_numbers_adjacent_to_symbols(lines, line_length, SYMBOLS)
+    part_numbers = get_sum_of_all_parts(all_symbols)
+    print(f"Part one: sum of all part numbers = {part_numbers}")
 
-    adjacent_to_gears = get_adjacent_to_gears(lines, line_length)
+    adjacent_to_gears = get_part_numbers_adjacent_to_symbols(lines, line_length, ["*"])
     valid_gears = get_gear_ratios(adjacent_to_gears)
     print(f"Part two: sum of all gear ratios = {sum(valid_gears)}")
 
